33740d343d71a4f81a983a6f9d9d686fe81d263c
Sagar <51394096+khunsagar@users.noreply.github.com>
2024-12-21 19:48:01 +0800

    updated config file to capture video

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.brighthorizons.com",
+    "video": true,
+    "videoUploadOnPasses": false,  
+    "videosFolder": "cypress/videos"
     // setupNodeEvents(on, config) {
     // }
   },
